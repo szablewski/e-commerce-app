3978274a9bef7b9e442113f65141d081bd839ce1
Bartosz Szablewski <bartosz@szablewski.hub.pl>
2024-07-19 16:09:26 +0200

    added base configuration product service

---
+++ services/product/src/test/java/com/szablewski/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szablewski.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
