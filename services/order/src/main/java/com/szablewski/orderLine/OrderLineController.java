718b909d0899f674eeacbb3df2d84eb2fa55b1cf
Bartosz Szablewski <bartosz@szablewski.hub.pl>
2024-08-06 09:27:22 +0200

    implement order and orderLine service

--- services/order/src/main/java/com/szablewski/orderLine/OrderLineController.java
+++ services/order/src/main/java/com/szablewski/orderLine/OrderLineController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/order-lines")
 @RequiredArgsConstructor
@@ -15,7 +17,7 @@ public class OrderLineController {
     private final OrderLineService service;
 
     @GetMapping("/order/{order-id}")
-    public ResponseEntity<OrderLineResponse> findByOrderId(@PathVariable("order-id")Integer orderId) {
+    public ResponseEntity<List<OrderLineResponse>> findByOrderId(@PathVariable("order-id")Integer orderId) {
         return ResponseEntity.ok(service.findByOrderId(orderId));
     }
 }
